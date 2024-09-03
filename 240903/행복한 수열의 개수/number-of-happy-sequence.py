4c16d92278500e32d90a79f31a0b24c1827d9558
Doomchit <80048007+DoomchitYJ@users.noreply.github.com>
2024-09-03 16:06:39 +0900

    Title: 행복한 수열의 개수, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240903/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240903/행복한 수열의 개수/number-of-happy-sequence.py
@@ -7,13 +7,20 @@ for _ in range(n):
 
 ans = 0
 
+# 1 예외처리
+if n == 1:
+    if m == 1:
+        ans = 2
+
 # 가로
 for i in range(n):
+    prev = arr[i][0]
     cnt = 1
-    for j in range(n-1):
-        if arr[i][j] == arr[i][j+1]:
+    for j in range(1, n):
+        if arr[i][j] == prev:
             cnt += 1
         else:
+            prev = arr[i][j]
             cnt = 1
         if cnt >= m:
             ans += 1
@@ -22,11 +29,13 @@ for i in range(n):
             
 # 세로
 for i in range(n):
+    prev = arr[0][i]
     cnt = 1
-    for j in range(n-1):
-        if arr[j][i] == arr[j+1][i]:
+    for j in range(1, n):
+        if arr[j][i] == prev:
             cnt += 1
         else:
+            prev = arr[j][i]
             cnt = 1
         if cnt >= m:
             ans += 1

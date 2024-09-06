53da405d5384e8823adb3d052eadb13fb88afcb7
Doomchit <80048007+DoomchitYJ@users.noreply.github.com>
2024-09-06 16:34:26 +0900

    Title: 삼각형 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240906/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static int n, t;
+    public static ArrayList belt = new ArrayList();
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        t = sc.nextInt();
+
+        for (int i=0; i<3; i++){
+            for (int j=0; j<n; j++){
+                belt.add(sc.nextInt());
+            }
+        }
+
+        int nt = t % (3*n);
+
+        ArrayList new_belt = new ArrayList();
+
+        for (int i=3*n - nt; i<3*n; i++){
+            new_belt.add(belt.get(i));
+        }
+        
+        for (int i=0; i<3*n - nt; i++){
+            new_belt.add(belt.get(i));
+        }
+
+        for (int i=0; i<3; i++){
+            for (int j=0; j<n; j++){
+                System.out.printf("%d ", new_belt.get(3*i + j));
+            }
+            System.out.printf("\n");
+        }
+    }
+}
+
+
+// 124
+// 593
+// 651
+
+// 1 2 4 5 9 3 6 5 1
+// 1 1 2 4 5 9 3 6 5

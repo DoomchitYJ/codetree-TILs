57b7495d83298eb167e8dedf765831e28a22e87c
Doomchit <80048007+DoomchitYJ@users.noreply.github.com>
2024-09-06 15:15:27 +0900

    Title: 기울어진 직사각형, Time: 219ms, Memory: 19MB, Status: Passed - CodeTree

--- 240906/기울어진 직사각형/slanted-rectangle.java
+++ 240906/기울어진 직사각형/slanted-rectangle.java
@@ -2,12 +2,42 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
+    public static int n;
+    public static int[][] arr = new int[20][20];
+
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+
+    public static int get_sum(int x, int y, int width, int height) {
+            int ret = 0;
+
+            int[] dx = {1, 1, -1, -1};
+            int[] dy = {1, -1, -1, 1};
+            int[] length = {width, height, width, height};
+
+            for(int i=0; i<4; i++){                
+                for (int j=0; j<length[i]; j++){
+                    x += dx[i];
+                    y += dy[i];
+
+                    if (!inRange(x, y)) {
+                        return 0;
+                    }
+
+                    ret += arr[x][y];    
+                } 
+            }
+
+            return ret;
+        }
+
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[][] arr = new int[n][n];
+        n = sc.nextInt();
 
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
@@ -18,45 +48,14 @@ public class Main {
         int ans = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                int tmp = get_sum(i, j, n, arr);
-                if (tmp > ans) ans = tmp;
+                for(int width=1; width<n; width++){
+                    for(int height=1; height<n; height++){
+                        int tmp = get_sum(i, j, width, height);
+                        if (tmp > ans) ans = tmp;
+                    }
+                }
             }
         }
         System.out.println(ans);
     }
-
-    public static int get_sum(int x, int y, int n, int[][] arr) {
-            int ret = 0;
-
-            for(int i=x+1, j=y; i<n-1; i++){
-                j--;
-                if(j > 0) {
-                    ret += arr[i][j-1];
-                    ret += arr[i+1][j];
-                } 
-            }
-
-            int[] dx = {-1, 0, 0, +1};
-            int[] dy = {0, -1, +1, 0};
-            
-            for(int i=0; i<4; i++){
-                int nx = x+dx[i];
-                int ny = y+dy[i];
-
-                if(0<=nx && nx<n && 0<=ny && ny<n) {
-                    ret += arr[nx][ny];
-                } else return 0;
-            }
-
-            for(int i=x, j=y+1; j<n-1; j++){
-                i--;
-                if(i > 0) {
-                    ret += arr[i][j+1];
-                    ret += arr[i-1][j];
-                }
-            }
-
-
-            return ret;
-        }
 }

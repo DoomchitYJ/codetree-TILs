8cf9195ebc033367f9a74a599548368d2216bb25
Doomchit <80048007+DoomchitYJ@users.noreply.github.com>
2024-09-08 22:38:18 +0900

    Title: 최적의 십자 모양 폭발, Time: 243ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240908/최적의 십자 모양 폭발/best-cross-shape-bomb.java
@@ -0,0 +1,87 @@
+// 20:14 ~
+
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int[][] origin = new int[50][50];
+    public static int[][] grid = new int[50][50];
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
+
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n; 
+    }
+    public static void bomb(int x, int y) {
+        int times = grid[x][y];
+        grid[x][y] = 0;
+        if (times == 1) return;
+
+        for (int i=0; i<4; i++){
+            int nx = x; int ny = y;
+            for (int j=0; j<times-1; j++){
+                nx += dx[i]; ny += dy[i];
+                if (inRange(nx, ny)) grid[nx][ny] = 0;
+            }
+        }
+    }
+    public static void arrange(){
+        for (int j=0; j<n; j++){
+            for (int i=n-1; i>0; i--){
+                if (grid[i][j] == 0){
+                    grid[i][j] = grid[i-1][j];
+                    grid[i-1][j] = 0;
+                }
+            }
+        }
+    }
+    public static int find(){
+        int cnt = 0;
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n-1; j++){
+                int a = grid[i][j]; int b = grid[i][j+1];
+                if (a != 0 && a == b) cnt++;
+            }
+        }
+        for (int j=0; j<n; j++){
+            for (int i=0; i<n-1; i++){
+                int a = grid[i][j]; int b = grid[i+1][j];
+                if (a != 0 && a == b) cnt++;
+            }
+        }
+        return cnt;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                origin[i][j] = sc.nextInt();
+            }
+        }
+
+        int max = 0;
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                
+                for (int d=0; d<n; d++){
+                    for (int q=0; q<n; q++){
+                        grid[d][q] = origin[d][q];
+                    }
+                } 
+                bomb(i, j);
+                arrange();
+                max = Math.max(max, find());
+
+                // for (int d=0; d<n; d++){
+                //     for (int q=0; q<n; q++){
+                //         System.out.print(grid[d][q]+" ");
+                //     }
+                //     System.out.println();
+                // } System.out.println();
+            }
+        }
+        
+        System.out.println(max);
+    }
+}

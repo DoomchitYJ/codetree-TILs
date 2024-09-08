2599cfc534b06d50e8aedd218c50df7a25ac3dc8
Doomchit <80048007+DoomchitYJ@users.noreply.github.com>
2024-09-08 23:18:35 +0900

    Title: 그래프 탐색, Time: 469ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/그래프 탐색/graph-traversal.java
+++ 240908/그래프 탐색/graph-traversal.java
@@ -3,9 +3,21 @@
 import java.util.*;
 
 public class Main {
-    public static int n, m;
+    public static int n, m, cnt;
     public static ArrayList[] graph = new ArrayList[1001];
-    public static int[] cnt = new int[1001];
+    public static boolean[] visited = new boolean[1001];
+
+    public static void dfs(int x){
+        visited[x] = true;
+
+        for (int i=0; i<graph[x].size(); i++){
+            int cur = (int)graph[x].get(i);
+            if (!visited[cur]) {
+                cnt++;
+                dfs(cur);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,16 +25,17 @@ public class Main {
         n = sc.nextInt();
         m = sc.nextInt();
 
-        for (int i=0; i<n; i++) graph[i] = new ArrayList();
+        for (int i=0; i<=n; i++) graph[i] = new ArrayList();
         
         for (int i=0; i<m; i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
 
             graph[a].add(b);
-            cnt[a]++;
+            graph[b].add(a);
         }
 
-        System.out.println(cnt[1]);
+        dfs(1);
+        System.out.print(cnt);
     }
 }

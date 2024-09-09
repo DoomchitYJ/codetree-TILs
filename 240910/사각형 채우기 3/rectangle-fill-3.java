16152abe6b9b4d978dd6aef591d18b99064281d7
Doomchit <80048007+DoomchitYJ@users.noreply.github.com>
2024-09-10 00:47:03 +0900

    Title: 사각형 채우기 3, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240910/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int[] dp = new int[1001];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        dp[1] = 2;
+        dp[2] = 7;
+
+        for (int i=3; i<=n; i++){
+            dp[i] = (2*dp[i-1] + 3*dp[i-2]) % 1000000007;
+            for (int j=i-3; j >=0; j--){
+                dp[i] = (dp[i] + 2*dp[j]) % 1000000007;
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}

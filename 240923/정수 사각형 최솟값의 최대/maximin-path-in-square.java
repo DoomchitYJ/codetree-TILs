5e1181f6c5c82b5b8c3db0f0e5f3e6b6bf7ddc2b
Doomchit <80048007+DoomchitYJ@users.noreply.github.com>
2024-09-23 23:22:35 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 325ms, Memory: 23MB, Status: Passed - CodeTree

--- 240923/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 240923/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -14,11 +14,11 @@ public class Main {
             }
         }
 
-        for (int i=0; i<n-1; i++) {
-            dp[0][i] = Math.min(dp[0][i], dp[0][i+1]);
+        for (int i=1; i<n; i++) {
+            dp[0][i] = Math.min(dp[0][i], dp[0][i-1]);
         }
-        for (int i=0; i<n-1; i++) {
-            dp[i][0] = Math.min(dp[i][0], dp[i+1][0]);
+        for (int i=1; i<n; i++) {
+            dp[i][0] = Math.min(dp[i][0], dp[i-1][0]);
         }
 
         for (int i=1; i<n; i++) {

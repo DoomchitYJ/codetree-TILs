f98177289288cfc2dc2e52946628cadb7cc083cd
Doomchit <80048007+DoomchitYJ@users.noreply.github.com>
2024-09-24 13:09:38 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1175ms, Memory: 75MB, Status: Passed - CodeTree

--- 240924/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240924/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -8,20 +8,17 @@ public class Main {
     public static int[] dx = {-1, 1, 0, 0};
     public static int[] dy = {0, 0, -1, 1};
     public static int gcnt = 0;
-    public static int this_cnt = 0;
 
-    public static void dfs(int x, int y, int cnt){
+    public static void dfs(int x, int y){
         visited[x][y] = true;
-        gcnt = Math.max(gcnt, cnt);
 
         for (int i=0; i<4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
             if (0 <= nx && nx < n && 0 <= ny && ny < n && grid[nx][ny] > grid[x][y]) {
-                if (visited[nx][ny] != true) dfs(nx, ny, cnt+1);
+                if (visited[nx][ny] != true) dfs(nx, ny);
                 dp[x][y] = Math.max(dp[x][y], dp[nx][ny]);
-                // System.out.printf("%d, %d ... %d, %d ... cnt = %d\n", x, y, nx, ny, dp[x][y]);
             }
         }
         dp[x][y]++;
@@ -38,7 +35,7 @@ public class Main {
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
                 if (!visited[i][j]) {
-                    dfs(i, j, 1);
+                    dfs(i, j);
                 }
             }
         }

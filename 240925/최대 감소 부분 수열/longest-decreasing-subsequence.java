08604309c88aaaf9e66aad74fc9641cd2061d917
Doomchit <80048007+DoomchitYJ@users.noreply.github.com>
2024-09-25 18:07:24 +0900

    Title: 최대 감소 부분 수열, Time: 244ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240925/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int[] arr = new int[1000];
+    public static int[] dp = new int[1000];
+    public static int ans = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        for (int i=0; i<n; i++) {
+            dp[i] = 1;
+            for (int j=0; j<n; j++) {
+                if (arr[i] < arr[j]) dp[i] = Math.max(dp[j]+1, dp[i]);
+            }
+            ans = Math.max(dp[i], ans);
+        }        
+
+        System.out.println(ans);
+    }
+}

4c13b7923f799857616fbe47d9dee963da85a81c
Doomchit <80048007+DoomchitYJ@users.noreply.github.com>
2024-09-27 17:11:43 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 423ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 240927/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    public static int n, ans;
+    public static int[] arr = new int[1000];
+    public static int[] dp1 = new int[1000];
+    public static int[] dp2 = new int[1000];
+    public static boolean check;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        for (int i=0; i<n; i++) {
+            dp1[i] = 1;
+
+            for (int j=0; j<i; j++) {
+                if (arr[j] < arr[i]) dp1[i] = Math.max(dp1[i], dp1[j]+1);
+            }
+
+            for (int j=i; j<n; j++) {
+                dp2[j] = 1;
+                for (int k=i; k<j; k++){
+                    if (arr[j] < arr[k]) {
+                        dp2[j] = Math.max(dp2[j], dp2[k]+1);
+                    }
+                }
+            }
+
+            int cnt1 = 0;
+            int cnt2 = 0;
+            for (int j=0; j<n; j++) {
+                cnt1 = Math.max(dp1[j], cnt1);
+                cnt2 = Math.max(dp2[j], cnt2);
+                ans = Math.max(cnt1+cnt2, ans);
+            }
+
+            // for (int j=0; j<n; j++) System.out.printf("%d ", dp1[j]);
+            // System.out.println();
+            // for (int j=0; j<n; j++) System.out.printf("%d ", dp2[j]);
+            // System.out.println();
+            dp2 = new int[1000];
+        }
+        System.out.println(ans-1);
+    }
+}

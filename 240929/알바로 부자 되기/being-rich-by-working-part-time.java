1042304520f77b09b611ddb705337bd75bdb5929
Doomchit <80048007+DoomchitYJ@users.noreply.github.com>
2024-09-29 18:48:17 +0900

    Title: 알바로 부자 되기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240929/알바로 부자 되기/being-rich-by-working-part-time.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static class Pair implements Comparable<Pair> {
+        long first, second;
+
+        public Pair() {}
+
+        public Pair(long x, long y) {
+            this.first = x; this.second = y;
+        }
+
+        @Override
+        public int compareTo(Pair p) {
+            return Long.compare(this.first, p.first);
+        }
+    }
+
+    public static int n;
+    public static long ans;
+    public static Pair[] arr = new Pair[1000];
+    public static int[] pay = new int[1000];
+    public static long[] dp = new long[1000];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            long s = sc.nextLong();
+            long e = sc.nextLong();
+            int p = sc.nextInt();
+            arr[i] = new Pair(s, e);
+            pay[i] = p;
+        }
+
+        for (int i=0; i<n; i++) {
+            dp[i] = pay[i];
+            long a = arr[i].first;
+            for (int j=0; j<i; j++) {
+                long b = arr[j].second;
+                if (a < b) dp[i] = Math.max(dp[j]+pay[j], dp[i]);
+            }
+        }
+        for (int i=0; i<n; i++) {
+            // System.out.printf("%d ", dp[i]);
+            ans = Math.max(ans, dp[i]);
+        }
+        // System.out.println();
+        System.out.println(ans);
+    }
+}
